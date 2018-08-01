3fac29fc196f9eabdc2115925fe89d2c11f94b89
Janice Collins <jcollins@google.com>
2018-08-01 09:47:29 -0700

    analyzer fix

--- test/all.dart
+++ test/all.dart
@@ -6,12 +6,9 @@ library services.all_test;
 
 import 'dart:async';
 
-import 'package:dart_services/src/sdk_manager.dart';
-
 import 'analysis_server_test.dart' as analysis_server_test;
 import 'api_classes_test.dart' as api_classes_test;
 import 'bench_test.dart' as bench_test;
-import 'common_server_test.dart' as common_server_test;
 import 'common_test.dart' as common_test;
 import 'compiler_test.dart' as compiler_test;
 import 'dartpad_server_test.dart' as dartpad_server_test;
