670e9a2b2be3f9a5ce29751c20d64b0ca1889f9b
Brett Morgan <brett.morgan@gmail.com>
2019-06-06 23:46:31 +1000

    Refactoring

--- lib/services_gae.dart
+++ lib/services_gae.dart
@@ -17,7 +17,7 @@ import 'src/dartpad_support_server.dart';
 import 'src/flutter_web.dart';
 
 const String _API = '/api';
-const String _livenessCheck = '/_ah/health';
+const String _healthCheck = '/_ah/health';
 const String _readynessCheck = '/_ah/ready';
 
 final Logger _logger = Logger('gae_server');
@@ -96,77 +96,85 @@ class GaeServer {
     request.response.headers.add('Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept');
 
-    // Explicitly handle an OPTIONS requests.
     if (request.method == 'OPTIONS') {
-      String requestedMethod =
-          request.headers.value('access-control-request-method');
-      int statusCode;
-      if (requestedMethod != null && requestedMethod.toUpperCase() == 'POST') {
-        statusCode = io.HttpStatus.ok;
-      } else {
-        statusCode = io.HttpStatus.badRequest;
-      }
-      request.response.statusCode = statusCode;
-      await request.response.close();
-      return;
-    }
-
-    if (request.uri.path == _readynessCheck) {
-      request.response.statusCode = io.HttpStatus.ok;
-      await request.response.close();
-      return;
-    }
-
-    if (request.uri.path == _livenessCheck) {
-      try {
-        final tempDir = await io.Directory.systemTemp.createTemp('healthz');
-        final file = await io.File('${tempDir.path}/livecheck.txt');
-        await file.writeAsString('testing123\n' * 1000, flush: true);
-        final stat = await file.stat();
-        if (stat.size > 10000) {
-          request.response.statusCode = io.HttpStatus.ok;
-        } else {
-          request.response.statusCode = io.HttpStatus.internalServerError;
-        }
-        await request.response.close();
-        await tempDir.delete(recursive: true);
-      } catch (e) {
-        _logger.severe('Failed to create temporary file: $e');
-        request.response.statusCode = io.HttpStatus.internalServerError;
-        await request.response.close();
-      }
-      return;
-    }
-
-    if (request.uri.path.startsWith(_API)) {
-      if (!discoveryEnabled) {
-        apiServer.enableDiscoveryApi();
-        discoveryEnabled = true;
-      }
-      // NOTE: We could read in the request body here and parse it similar to
-      // the _parseRequest method to determine content-type and dispatch to e.g.
-      // a plain text handler if we want to support that.
-      rpc.HttpApiRequest apiRequest =
-          rpc.HttpApiRequest.fromHttpRequest(request);
-
-      // Dartpad sends data as plain text, we need to promote this to
-      // application/json to ensure that the rpc library processes it correctly
-      try {
-        apiRequest.headers['content-type'] = 'application/json; charset=utf-8';
-        var apiResponse = await apiServer.handleHttpApiRequest(apiRequest);
-        await rpc.sendApiResponse(apiResponse, request.response);
-      } catch (e) {
-        // This should only happen in the case where there is a bug in the rpc
-        // package. Otherwise it always returns an HttpApiResponse.
-        _logger.warning('Failed with error: $e when trying to call '
-            'method at \'${request.uri.path}\'.');
-        request.response.statusCode = io.HttpStatus.internalServerError;
-        await request.response.close();
-      }
+      await _processOptionsRequest(request);
+    } else if (request.uri.path == _readynessCheck) {
+      await _processReadynessRequest(request);
+    } else if (request.uri.path == _healthCheck) {
+      await _processHealthRequest(request);
+    } else if (request.uri.path.startsWith(_API)) {
+      await _processApiRequest(request);
     } else {
-      request.response.statusCode = io.HttpStatus.notFound;
+      await _processDefaultRequest(request);
+    }
+  }
+
+  Future<void> _processOptionsRequest(io.HttpRequest request) async {
+    String requestedMethod =
+        request.headers.value('access-control-request-method');
+    int statusCode;
+    if (requestedMethod != null && requestedMethod.toUpperCase() == 'POST') {
+      statusCode = io.HttpStatus.ok;
+    } else {
+      statusCode = io.HttpStatus.badRequest;
+    }
+    request.response.statusCode = statusCode;
+    await request.response.close();
+  }
+
+  Future _processReadynessRequest(io.HttpRequest request) async {
+    request.response.statusCode = io.HttpStatus.ok;
+    await request.response.close();
+  }
+
+  Future _processHealthRequest(io.HttpRequest request) async {
+    try {
+      final tempDir = await io.Directory.systemTemp.createTemp('healthz');
+      final file = await io.File('${tempDir.path}/livecheck.txt');
+      await file.writeAsString('testing123\n' * 1000, flush: true);
+      final stat = await file.stat();
+      if (stat.size > 10000) {
+        request.response.statusCode = io.HttpStatus.ok;
+      } else {
+        request.response.statusCode = io.HttpStatus.internalServerError;
+      }
+      await tempDir.delete(recursive: true);
+    } catch (e) {
+      _logger.severe('Failed to create temporary file: $e');
+      request.response.statusCode = io.HttpStatus.internalServerError;
+    }
+    await request.response.close();
+  }
+
+  Future _processApiRequest(io.HttpRequest request) async {
+    if (!discoveryEnabled) {
+      apiServer.enableDiscoveryApi();
+      discoveryEnabled = true;
+    }
+    // NOTE: We could read in the request body here and parse it similar to
+    // the _parseRequest method to determine content-type and dispatch to e.g.
+    // a plain text handler if we want to support that.
+    rpc.HttpApiRequest apiRequest = rpc.HttpApiRequest.fromHttpRequest(request);
+
+    // Dartpad sends data as plain text, we need to promote this to
+    // application/json to ensure that the rpc library processes it correctly
+    try {
+      apiRequest.headers['content-type'] = 'application/json; charset=utf-8';
+      var apiResponse = await apiServer.handleHttpApiRequest(apiRequest);
+      await rpc.sendApiResponse(apiResponse, request.response);
+    } catch (e) {
+      // This should only happen in the case where there is a bug in the rpc
+      // package. Otherwise it always returns an HttpApiResponse.
+      _logger.warning('Failed with error: $e when trying to call '
+          'method at \'${request.uri.path}\'.');
+      request.response.statusCode = io.HttpStatus.internalServerError;
       await request.response.close();
     }
+  }
+
+  Future _processDefaultRequest(io.HttpRequest request) async {
+    request.response.statusCode = io.HttpStatus.notFound;
+    await request.response.close();
   }
 }
 
