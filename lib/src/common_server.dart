882821fb46d3b140843e67a700f69a04c18aea42
Brett Morgan <brett.morgan@gmail.com>
2020-02-11 20:39:03 -0800

    Hide the implementation

--- lib/src/common_server.dart
+++ lib/src/common_server.dart
@@ -21,29 +21,29 @@ abstract class ServerContainer {
 
 @ApiClass(name: 'dartservices', version: 'v1')
 class CommonServer {
-  final CommonServerImpl impl;
+  final CommonServerImpl _impl;
 
   bool get analysisServersRunning =>
-      impl.analysisServer.analysisServer != null &&
-      impl.flutterAnalysisServer.analysisServer != null;
+      _impl.analysisServer.analysisServer != null &&
+      _impl.flutterAnalysisServer.analysisServer != null;
 
-  bool get running => impl.running;
+  bool get running => _impl.running;
 
   CommonServer(
     String sdkPath,
     FlutterWebManager flutterWebManager,
     ServerContainer container,
     ServerCache cache,
-  ) : impl = CommonServerImpl(sdkPath, flutterWebManager, container, cache);
+  ) : _impl = CommonServerImpl(sdkPath, flutterWebManager, container, cache);
 
-  Future<void> init() async => impl.init();
+  Future<void> init() async => _impl.init();
 
   Future<void> warmup({bool useHtml = false}) async =>
-      impl.warmup(useHtml: useHtml);
+      _impl.warmup(useHtml: useHtml);
 
-  Future<void> restart() async => impl.restart();
+  Future<void> restart() async => _impl.restart();
 
-  Future<dynamic> shutdown() => impl.shutdown();
+  Future<dynamic> shutdown() => _impl.shutdown();
 
   @ApiMethod(
       method: 'POST',
@@ -52,7 +52,7 @@ class CommonServer {
           'Analyze the given Dart source code and return any resulting '
           'analysis errors or warnings.')
   Future<AnalysisResults> analyze(SourceRequest request) =>
-      _convertBadRequest(() => impl.analyze(request));
+      _convertBadRequest(() => _impl.analyze(request));
 
   @ApiMethod(
       method: 'POST',
@@ -60,7 +60,7 @@ class CommonServer {
       description: 'Compile the given Dart source code and return the '
           'resulting JavaScript; this uses the dart2js compiler.')
   Future<CompileResponse> compile(CompileRequest request) =>
-      _convertBadRequest(() => impl.compile(request));
+      _convertBadRequest(() => _impl.compile(request));
 
   @ApiMethod(
       method: 'POST',
@@ -68,7 +68,7 @@ class CommonServer {
       description: 'Compile the given Dart source code and return the '
           'resulting JavaScript; this uses the DDC compiler.')
   Future<CompileDDCResponse> compileDDC(CompileRequest request) =>
-      _convertBadRequest(() => impl.compileDDC(request));
+      _convertBadRequest(() => _impl.compileDDC(request));
 
   @ApiMethod(
       method: 'POST',
@@ -76,21 +76,21 @@ class CommonServer {
       description:
           'Get the valid code completion results for the given offset.')
   Future<CompleteResponse> complete(SourceRequest request) =>
-      _convertBadRequest(() => impl.complete(request));
+      _convertBadRequest(() => _impl.complete(request));
 
   @ApiMethod(
       method: 'POST',
       path: 'fixes',
       description: 'Get any quick fixes for the given source code location.')
   Future<FixesResponse> fixes(SourceRequest request) =>
-      _convertBadRequest(() => impl.fixes(request));
+      _convertBadRequest(() => _impl.fixes(request));
 
   @ApiMethod(
       method: 'POST',
       path: 'assists',
       description: 'Get assists for the given source code location.')
   Future<AssistsResponse> assists(SourceRequest request) =>
-      _convertBadRequest(() => impl.assists(request));
+      _convertBadRequest(() => _impl.assists(request));
 
   @ApiMethod(
       method: 'POST',
@@ -99,7 +99,7 @@ class CommonServer {
           'If an offset is supplied in the request, the new position for that '
           'offset in the formatted code will be returned.')
   Future<FormatResponse> format(SourceRequest request) =>
-      _convertBadRequest(() => impl.format(request));
+      _convertBadRequest(() => _impl.format(request));
 
   @ApiMethod(
       method: 'POST',
@@ -107,21 +107,21 @@ class CommonServer {
       description: 'Return the relevant dartdoc information for the element at '
           'the given offset.')
   Future<DocumentResponse> document(SourceRequest request) =>
-      _convertBadRequest(() => impl.document(request));
+      _convertBadRequest(() => _impl.document(request));
 
   @ApiMethod(
       method: 'GET',
       path: 'version',
       description: 'Return the current SDK version for DartServices.')
-  Future<VersionResponse> version() => _convertBadRequest(() => impl.version());
+  Future<VersionResponse> version() => _convertBadRequest(() => _impl.version());
 
-  Future<String> checkCache(String query) => impl.checkCache(query);
+  Future<String> checkCache(String query) => _impl.checkCache(query);
 
   Future<void> setCache(String query, String result) =>
-      impl.setCache(query, result);
+      _impl.setCache(query, result);
 
   AnalysisServerWrapper getCorrectAnalysisServer(String source) =>
-      impl.getCorrectAnalysisServer(source);
+      _impl.getCorrectAnalysisServer(source);
 }
 
 Future<T> _convertBadRequest<T>(Future<T> Function() fun) async {
