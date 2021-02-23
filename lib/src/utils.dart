69e5863f9f8ab38a3b670e266289433674e450af
John Ryan <ryjohn@google.com>
2021-02-22 16:17:55 -0800

    use package:expected_output for path stripping tests

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,6 +1,14 @@
 import 'package:path/path.dart' as path;
 
 String stripFilePaths(String s) {
-  return s.replaceAllMapped(
-      RegExp(r'/\S*'), (match) => path.basename(match.group(0)));
+  return s.replaceAllMapped(RegExp(r'/\S*'), (match) {
+    final urlString = match.group(0);
+    final isDartPath = path.split(urlString).contains('lib');
+    final basename = path.basename(urlString);
+
+    if (isDartPath) {
+      return path.join('dart:core', basename);
+    }
+    return basename;
+  });
 }
--- test/utils_test.dart
+++ test/utils_test.dart
@@ -1,26 +1,15 @@
+library utils.tests;
 import 'package:dart_services/src/utils.dart';
+import 'package:expected_output/expected_output.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('utils.stripFilePaths', () {
-    test('Strips temporary directory paths', () {
-      var errorMessage = 'List is defined in '
-          '/var/folders/4p/y54w9nqj0_n6ryqwn7lxqz6800m6cw/T/'
-          'DartAnalysisWrapperintLAw/main.dart';
-      var expected = 'List is defined in main.dart';
-      expect(stripFilePaths(errorMessage), equals(expected));
-    });
-
-    test('Strips temporary directory paths', () {
-      var errorMessage = "The argument type 'List<int> (where List is defined "
-          "in /Users/username/sdk/dart/2.10.5/lib/core/list.dart)' can't be "
-          "assigned to the parameter type 'List<int> (where List is defined "
-          "in /var/folders/4p/tmp/T/DartAnalysisWrapperintLAw/main.dart)'.";
-      var expected = "The argument type 'List<int> (where List is defined "
-          "in list.dart)' can't be "
-          "assigned to the parameter type 'List<int> (where List is defined "
-          "in main.dart)'.";
-      expect(stripFilePaths(errorMessage), equals(expected));
-    });
+    for (var dataCase in dataCasesUnder(library: #utils.tests)) {
+      test(dataCase.testDescription, () {
+        var actualOutput = stripFilePaths(dataCase.input);
+        expect(actualOutput, equals(dataCase.expectedOutput));
+      });
+    }
   });
 }
