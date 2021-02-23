1c47f511bf2d29288df9096a709c4a2810c91d88
John Ryan <ryjohn@google.com>
2021-02-23 08:34:20 -0800

    Ensure dart:core paths are kept intact.

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,16 +1,34 @@
 import 'package:path/path.dart' as path;
 
-String stripFilePaths(String s) {
-  return s.replaceAllMapped(RegExp(r'(?:package:?)?[a-z]*\/\S*'), (match) {
+/// Matches any path in the string and replaces the part of the path before the
+/// last separator with either dart:core, package:flutter, or removes it.
+///
+/// ## Examples:
+///
+/// "Unused import: '/path/foo.dart'" -> "Unused import: 'foo.dart'"
+///
+/// "Unused import: '/path/to/dart/lib/world.dart'" -> "Unused import:
+/// 'dart:core/world.dart'"
+///
+/// "Unused import: 'package:flutter/material.dart'" -> "Unused import:
+/// 'package:flutter/material.dart'"
+String stripFilePaths(String str) {
+  final regex = RegExp(r'(?:dart:core?)?(?:package:?)?[a-z]*\/\S*');
+  return str.replaceAllMapped(regex, (match) {
     final urlString = match.group(0);
     final pathComponents = path.split(urlString);
     final isDartPath = pathComponents.contains('lib');
     final isFlutterPath = isDartPath && pathComponents.contains('flutter');
     final isPackagePath = urlString.contains('package:');
+    final isDartCorePath = urlString.contains('dart:core');
     final basename = path.basename(urlString);
 
     if (isFlutterPath) {
       return path.join('package:flutter', basename);
+    }
+
+    if (isDartCorePath) {
+      return urlString;
     }
 
     if (isDartPath) {
