d31bf8988d5d8df5788399d671bdc3d6b6b1c350
John Ryan <ryjohn@google.com>
2021-02-22 16:32:56 -0800

    Keep package:paths intact when stripping URL paths.

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,13 +1,18 @@
 import 'package:path/path.dart' as path;
 
 String stripFilePaths(String s) {
-  return s.replaceAllMapped(RegExp(r'/\S*'), (match) {
+  return s.replaceAllMapped(RegExp(r'(?:package:?)?[a-z]*\/\S*'), (match) {
     final urlString = match.group(0);
     final isDartPath = path.split(urlString).contains('lib');
+    final isPackagePath = urlString.contains('package:');
     final basename = path.basename(urlString);
 
     if (isDartPath) {
       return path.join('dart:core', basename);
+    }
+
+    if (isPackagePath) {
+      return urlString;
     }
     return basename;
   });
